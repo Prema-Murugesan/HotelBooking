da2a1da41b429ad52a3539ba54f3392a2b3528dd
Prema-Murugesan <premaeceskct@gmail.com>
2026-02-06 15:01:28 +0530

    Update and rename stepdefinitions.java to Stepdefinitions.java

--- booking/src/test/java/stepdefinitions/Stepdefinitions.java
+++ booking/src/test/java/stepdefinitions/Stepdefinitions.java
@@ -23,7 +23,7 @@ import io.restassured.response.Response;
 import io.restassured.response.ResponseBodyExtractionOptions;
 import io.restassured.response.ResponseOptions;
 
-public class stepdefinitions extends utilities{
+public class Stepdefinitions extends Utilities{
 	    private Response response;
 	    private String username;
 	    private String password;
